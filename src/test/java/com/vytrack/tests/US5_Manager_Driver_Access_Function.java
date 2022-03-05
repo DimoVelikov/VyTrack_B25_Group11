c8f4a93cefe69d8ad9c512c66adc8778cfdc2573
LenaPC <elenachubukova@yahoo.com>
2022-03-05 12:17:59 -0500

    US-5 - both Test Cases are done. TC2 failed, bug.

--- src/test/java/com/vytrack/tests/US5_Manager_Driver_Access_Function.java
+++ src/test/java/com/vytrack/tests/US5_Manager_Driver_Access_Function.java
@@ -1,4 +1,210 @@
 package com.vytrack.tests;
 
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.Driver;
+import com.vytrack.utilities.VyTrack_Utilities;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.reporters.jq.Model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class US5_Manager_Driver_Access_Function {
+
+    /*
+    USER STORY: As a user, I want to view columns on the Vehicle models page.(web-table).
+
+    AC:
+        AC #1: The store manager and sales manager should see 10 columns on the Vehicle Model page.
+
+        Expected Column names:
+        MODEL NAME, MAKE, CAN BE REQUESTED, CVVI, CO2 FEE (/MONTH), COST (DEPRECIATED),
+        TOTAL COST (DEPRECIATED), CO2 EMISSIONS, FUEL TYPE, VENDORS
+
+         AC #2: Drivers should not able to access the Vehicle Model page,
+         users should see “You do not have permission to perform this action.”
+     */
+
+    // -----------------------------SALES MANAGER ACCESS---------------------------------------
+    @Test
+    public void US5_AC1_Manager_Access_To_VehiclesModel_Tab() {
+
+
+        /*
+        AC #1: The store manager and sales manager should see 10 columns on the Vehicle Model page.
+
+        Expected Column names:
+        MODEL NAME, MAKE, CAN BE REQUESTED, CVVI, CO2 FEE (/MONTH), COST (DEPRECIATED),
+                TOTAL COST (DEPRECIATED), CO2 EMISSIONS, FUEL TYPE, VENDORS
+*/
+
+        // STEP 1: User go to homepage
+        Driver.getDriver().get("https://app.vytrack.com/user/login");
+
+        // User enters username
+        Driver.getDriver().findElement(By.xpath("//input[@type='text']")).sendKeys("salesmanager141");
+        BrowserUtils.sleep(2);
+        // User enters password
+        Driver.getDriver().findElement(By.xpath("//input[@type='password']")).sendKeys("UserUser123");
+        BrowserUtils.sleep(2);
+        // User clicks SIGN IN button
+        Driver.getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+        BrowserUtils.sleep(2);
+
+
+        // STEP 2: System should display 'Fleet' tab / module
+
+        // Locate 'Fleet' tab/module
+        WebElement moduleTab = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[2]"));
+
+        String expectedModuleName = "Fleet";
+        String actualModuleName = moduleTab.getText();
+
+        // Assert system displays 'Fleet' tab/module
+        Assert.assertEquals(actualModuleName, expectedModuleName);
+
+
+        // STEP 3: Click Fleet tab/module
+
+        moduleTab.click();
+        BrowserUtils.sleep(4);
+
+        // System should show dropdown with Vehicles Model tab/module
+
+        // Verify system shows Vehicles Model tab/module
+        WebElement vehiclesModelTab = Driver.getDriver().findElement(By.xpath("//span[.='Vehicles Model']"));
+
+        String expectedVehiclesModelTab = "Vehicles Model";
+        String actualVehiclesModelTab = vehiclesModelTab.getText();
+
+        // Assert system displays 'Vehicles Model' tab/module
+        Assert.assertEquals(actualVehiclesModelTab, expectedVehiclesModelTab);
+        BrowserUtils.sleep(3);
+
+
+        // STEP 3:
+
+        // Click Vehicles Model tab / module
+
+        vehiclesModelTab.click();
+        BrowserUtils.sleep(4);
+
+        //System should display 10 columns on the Vehicle Model page
+        //Expected Column names:
+        //MODEL NAME, MAKE, CAN BE REQUESTED, CVVI, CO2 FEE (/MONTH), COST (DEPRECIATED),
+        // TOTAL COST (DEPRECIATED), CO2 EMISSIONS, FUEL TYPE, VENDORS
+
+        WebElement modelNameColumn = Driver.getDriver().findElement(By.xpath("//span[.='Model Name']"));
+        modelNameColumn.isDisplayed();
+
+        WebElement makeColumn = Driver.getDriver().findElement(By.xpath("//span[.='Make']"));
+        modelNameColumn.isDisplayed();
+
+        WebElement canBeRequestedColumn = Driver.getDriver().findElement(By.xpath("//span[.='Can be requested']"));
+        modelNameColumn.isDisplayed();
+
+        WebElement cvviColumn = Driver.getDriver().findElement(By.xpath("//span[.='CVVI']"));
+        modelNameColumn.isDisplayed();
+
+        WebElement co2FeeColumn = Driver.getDriver().findElement(By.xpath("//span[.='CO2 Fee (/month)']"));
+        modelNameColumn.isDisplayed();
+
+        WebElement costNameColumn = Driver.getDriver().findElement(By.xpath("//span[.='Cost (Depreciated)']"));
+        modelNameColumn.isDisplayed();
+
+        WebElement totalCostNameColumn = Driver.getDriver().findElement(By.xpath("//span[.='Total Cost (Depreciated)']"));
+        modelNameColumn.isDisplayed();
+
+        WebElement co2EmissionsNameColumn = Driver.getDriver().findElement(By.xpath("//span[.='CO2 Emissions']"));
+        modelNameColumn.isDisplayed();
+
+        WebElement fuelTypeNameColumn = Driver.getDriver().findElement(By.xpath("//span[.='Fuel Type']"));
+        modelNameColumn.isDisplayed();
+
+        WebElement vendorsColumn = Driver.getDriver().findElement(By.xpath("//span[.='Vendors']"));
+        modelNameColumn.isDisplayed();
+
+    }
+
+//-----------------------------------------TRUCK DRIVER ACCESS-----------------------------------------
+    @Test
+    public void US5_AC2_Drivers_access(){
+
+        /*
+        AC #2: Drivers should NOT able to access the Vehicle Model page,
+        users should see “You do not have permission to perform this action.”
+         */
+
+
+        // STEP 1: User go to homepage
+        Driver.getDriver().get("https://app.vytrack.com/user/login");
+
+        // User enters username
+        Driver.getDriver().findElement(By.xpath("//input[@type='text']")).sendKeys("user177");
+        BrowserUtils.sleep(2);
+        // User enters password
+        Driver.getDriver().findElement(By.xpath("//input[@type='password']")).sendKeys("UserUser123");
+        BrowserUtils.sleep(2);
+        // User clicks SIGN IN button
+        Driver.getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+        BrowserUtils.sleep(2);
+
+
+        // STEP 2: System should display 'Fleet' tab / module
+
+        // Locate 'Fleet' tab/module
+        WebElement moduleTab = Driver.getDriver().findElement(By.xpath("((//span[@class='title title-level-1'])[1])"));
+
+        String expectedModuleName = "Fleet";
+        String actualModuleName = moduleTab.getText();
+
+        // Assert system displays 'Fleet' tab/module
+        Assert.assertEquals(actualModuleName, expectedModuleName);
+
+
+        // STEP 3: Click Fleet tab/module
+
+        moduleTab.click();
+        BrowserUtils.sleep(4);
+
+        // System should show dropdown with Vehicles Model tab/module
+
+        // Verify system shows Vehicles Model tab/module
+        WebElement vehiclesModelTab = Driver.getDriver().findElement(By.xpath("//span[.='Vehicles Model']"));
+
+        String expectedVehiclesModelTab = "Vehicles Model";
+        String actualVehiclesModelTab = vehiclesModelTab.getText();
+
+        // Assert system displays 'Vehicles Model' tab/module
+        Assert.assertEquals(actualVehiclesModelTab, expectedVehiclesModelTab);
+        BrowserUtils.sleep(3);
+
+
+        // STEP 3:
+
+        // Click Vehicles Model tab / module
+
+        vehiclesModelTab.click();
+        BrowserUtils.sleep(4);
+
+        // System should show error message:
+        //“You do not have permission to perform this action.”
+
+        WebElement errorMessage = Driver.getDriver().findElement(By.xpath("(//div[@class='alert alert-error fade in top-messages '])[1]"));
+
+        String expectedErrorMessage = "You do not have permission to perform this action.";
+        String actualErrorMessage = errorMessage.getText();
+
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage, "Invalid error message detected");
+
+
+
+
+    }
+
+
 }
+
