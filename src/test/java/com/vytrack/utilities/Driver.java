ea3cf5a2ea23d56a8c2ef8c1bbcc95dfc6de1af3
LenaPC <elenachubukova@yahoo.com>
2022-03-05 21:58:53 -0500

    US-13 - Driver Class added

---
+++ src/test/java/com/vytrack/utilities/Driver.java
@@ -0,0 +1,70 @@
+package com.vytrack.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+    // #1- Constructor -> access to the obj only in this class
+    private Driver() {
+
+    }
+
+    // #2- private - access is limited, static - we can use it in static method
+    private static WebDriver driver;
+
+// #3 - getter method // will return same driver instance  when we call it
+    public static WebDriver getDriver() {
+
+        if (driver == null){
+
+            String browserType = ConfigurationReader.getProperty("browser"); // gets from conf.properties
+
+            // we read our browser type from configuration.properties.
+            // This way we can control which browser is opened from outside our code
+
+            switch (browserType){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
+                case "opera":
+                    WebDriverManager.operadriver().setup();
+                    driver = new OperaDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+            }
+        }
+        return driver;
+    }
+
+}
