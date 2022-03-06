5b9023458368afb58b7151f1a2f00cb20e1ac8f5
LenaPC <elenachubukova@yahoo.com>
2022-03-05 22:00:26 -0500

    US-13 - US13 - AC1 is created

--- src/test/java/com/vytrack/tests/US13_ManageFiltersOn_VehicleCostsPage.java
+++ src/test/java/com/vytrack/tests/US13_ManageFiltersOn_VehicleCostsPage.java
@@ -1,4 +1,12 @@
 package com.vytrack.tests;
 
+import com.vytrack.utilities.Driver;
+import org.testng.annotations.Test;
+
 public class US13_ManageFiltersOn_VehicleCostsPage {
+
+    @Test
+    public void US13_ManageFilters_OnVehicleCostsPage(){
+
+    }
 }
