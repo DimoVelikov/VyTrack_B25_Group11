740cd36887af08dd6d035efbb45c4c44b5831235
Lilya <liya.riman@gmail.com>
2022-03-13 16:07:31 -0400

    Refactoring login utilities

--- src/test/java/com/vytrack/utilities/VytrackUtils.java
+++ src/test/java/com/vytrack/utilities/VytrackUtils.java
@@ -1,6 +1,11 @@
 package com.vytrack.utilities;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class VytrackUtils {
 
@@ -19,41 +24,40 @@ public class VytrackUtils {
     }
 
     public static void loginAsDriver() {
-        //go to website
-        Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
-        // pass username
-        // Driver.getDriver().findElement(By.cssSelector("#prependedInput"));
-        Driver.getDriver().findElement(By.id("prependedInput")).sendKeys(ConfigurationReader.getProperty("driver_username"));
-        //pass password
-        BrowserUtils.sleep(2);
-        Driver.getDriver().findElement(By.cssSelector("#prependedInput2")).sendKeys(ConfigurationReader.getProperty("driver_password"));
-        //click login button
-        BrowserUtils.sleep(1);
-        Driver.getDriver().findElement(By.tagName("button")).click();
+        login(ConfigurationReader.getProperty("driver_username"),ConfigurationReader.getProperty("driver_password"));
+
     }
 
     public static void loginAsStoreManger() {
-        //go to website
-        Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
-        // pass username
-        // Driver.getDriver().findElement(By.cssSelector("#prependedInput"));
-        Driver.getDriver().findElement(By.id("prependedInput")).sendKeys(ConfigurationReader.getProperty("store_manager_username"));
-        //pass password
-        Driver.getDriver().findElement(By.cssSelector("#prependedInput2")).sendKeys(ConfigurationReader.getProperty("store_manager_password"));
-        //click login button
-        Driver.getDriver().findElement(By.tagName("button")).click();
+        login(ConfigurationReader.getProperty("store_manager_username"),ConfigurationReader.getProperty("store_manager_password"));
+
     }
 
     public static void loginAsSalesManager() {
-        //go to website
-        Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
-        // pass username
-        // Driver.getDriver().findElement(By.cssSelector("#prependedInput"));
-        Driver.getDriver().findElement(By.id("prependedInput")).sendKeys(ConfigurationReader.getProperty("sales_manager_username"));
-        //pass password
-        Driver.getDriver().findElement(By.cssSelector("#prependedInput2")).sendKeys(ConfigurationReader.getProperty("sales_manager_password"));
-        //click login button
-        Driver.getDriver().findElement(By.tagName("button")).click();
+        login(ConfigurationReader.getProperty("sales_manager_username"),ConfigurationReader.getProperty("sales_manager_password"));
+
+    }
+
+    public static void waitTillLoaderMaskDisappear(){
+        try {
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(),20);
+            WebElement loaderMask = Driver.getDriver().findElement(By.cssSelector("div[class='loader-mask shown']"));
+            wait.until(ExpectedConditions.invisibilityOf(loaderMask));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void locateModule (String tabName, String moduleName){
+
+        String fleetTabLocator = "//span[normalize-space()='"+tabName+"' and contains(@class,'title title-level-1')]";
+        WebElement fleetTabElement = Driver.getDriver().findElement(By.xpath(fleetTabLocator));
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(fleetTabElement).perform();
+        waitTillLoaderMaskDisappear();
+        String vehicleOdometerLocator = "//span[normalize-space()='"+moduleName+"' and contains(@class,'title title-level-2')]";
+        WebElement vehicleOdometerElement = Driver.getDriver().findElement(By.xpath(vehicleOdometerLocator));
+        vehicleOdometerElement.click();
     }
 
 }
